c516a23bc9b0078dadc2628625dceb7e241e1a46
macdarat <makubyz@gmail.com>
2017-11-15 17:16:56 +0000

    Add project files.

---
+++ GithubReader/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Octokit;
+
+namespace GithubReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+            var github = new GitHubClient(new ProductHeaderValue("GithubReader"));
+        }
+    }
+}
