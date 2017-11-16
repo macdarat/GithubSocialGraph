d88dccdf1923094a1cf1e8456deeae85a4e76675
macdarat <makubyz@gmail.com>
2017-11-16 12:40:49 +0000

    Add code to print info on user's repositories

--- GithubReader/Program.cs
+++ GithubReader/Program.cs
@@ -16,12 +16,22 @@ namespace GithubReader
             Console.ReadLine();
         }
 
+        /**
+         * Print details about a given user on Github
+         */
         async static void PrintUser(string uname)
         {
             var github = new GitHubClient(new ProductHeaderValue("GithubReader"));
             var user = await github.User.Get(uname);
             Console.WriteLine(uname + "'s bio:" + user.Bio + uname + "'s total public repos:" + user.PublicRepos);
             ApiInfo apiInfo = github.GetLastApiInfo();
+
+            var repos = await github.Repository.GetAllForUser(uname);
+            foreach (Repository r in repos)
+            {
+                Console.WriteLine("Repository {0}, {1}, language {2}, last updated {3}, created at {4}", 
+                    r.FullName, r.Description, r.Language, r.UpdatedAt, r.CreatedAt);
+            }
 
             var rateLimit = apiInfo?.RateLimit;
             var reqPerHour = rateLimit?.Limit;
