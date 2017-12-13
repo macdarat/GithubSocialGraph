98edd3c16611541ed6b4ca62f1787cd02ae7cb40
macdarat <makubyz@gmail.com>
2017-12-13 22:18:50 +0000

    Now calculates averages, makes a seperate average csv

--- GithubReader/Program.cs
+++ GithubReader/Program.cs
@@ -21,8 +21,6 @@ namespace GithubReader
             var basicAuth = new Credentials(username, password);
             client.Credentials = basicAuth;
 
-            
-
             FetchRepoDetails(client).Wait();
             //PrintUser(client).Wait();
             Console.ReadKey();
@@ -42,59 +40,83 @@ namespace GithubReader
 
             string filePath = "..\\..\\..\\repositories.csv"; //places file 3 directories up, in proper place
             File.Create(filePath);      //create file to store results in
-            
+            string averageFP = "..\\..\\..\\repoAverages.csv";
+            File.Create(averageFP);    //file for the mean values
+
             searchReq.Language = Language.CSharp;
             var cSharpRepos = await github.Search.SearchRepo(searchReq);
             string line = "language,OpenIssuesCount,ForksCount,Size\n";     //add first row of csv
             File.AppendAllText(filePath, line);
-            OutputRepoDetails(cSharpRepos, "C#", filePath);
+            line = "language,OpenIssuesAverage,ForksAverage,SizeAverage\n";     //add first row of csv
+            File.AppendAllText(averageFP, line);
+
+            OutputRepoDetails(cSharpRepos, "C#", filePath, averageFP);
             
             searchReq.Language = Language.C;
             var cRepos = await github.Search.SearchRepo(searchReq);
-            OutputRepoDetails(cRepos, "C", filePath);
+            OutputRepoDetails(cRepos, "C", filePath, averageFP);
 
             searchReq.Language = Language.Java;
             var javaRepos = await github.Search.SearchRepo(searchReq);
-            OutputRepoDetails(javaRepos, "Java", filePath);
+            OutputRepoDetails(javaRepos, "Java", filePath, averageFP);
 
             searchReq.Language = Language.Ruby;
             var rubyRepos = await github.Search.SearchRepo(searchReq);
-            OutputRepoDetails(rubyRepos, "Ruby", filePath);
+            OutputRepoDetails(rubyRepos, "Ruby", filePath, averageFP);
 
             searchReq.Language = Language.JavaScript;
             var javascriptRepos = await github.Search.SearchRepo(searchReq);
-            OutputRepoDetails(javascriptRepos, "JavaScript", filePath);
+            OutputRepoDetails(javascriptRepos, "JavaScript", filePath, averageFP);
 
             searchReq.Language = Language.Haskell;
             var haskellRepos = await github.Search.SearchRepo(searchReq);
-            OutputRepoDetails(haskellRepos, "Haskell", filePath);
+            OutputRepoDetails(haskellRepos, "Haskell", filePath, averageFP);
 
             searchReq.Language = Language.Python;
             var pythonRepos = await github.Search.SearchRepo(searchReq);
-            OutputRepoDetails(javaRepos, "Python", filePath);
+            OutputRepoDetails(javaRepos, "Python", filePath, averageFP);
 
             searchReq.Language = Language.Assembly;
             var asmRepos = await github.Search.SearchRepo(searchReq);
-            OutputRepoDetails(asmRepos, "Assembly", filePath);
+            OutputRepoDetails(asmRepos, "Assembly", filePath, averageFP);
 
             PrintReqsRemaining(github);
         }
 
         /** <summary> Outputs repos details to console, then appends relevant info for each repo into a csv file </summary>*/
-        public static void OutputRepoDetails(SearchRepositoryResult sResult, string language, string filePath)
+        public static void OutputRepoDetails(SearchRepositoryResult sResult, string language, string filePath, string averageFP)
         {
             if (sResult.IncompleteResults)
             {
                 Console.WriteLine("INCOMPLETE RESULTS FOR {0}", language);
             }
             var languageResults = sResult.Items;
+
+            int openIssuesAv = 0;
+            int ForksCountAv = 0;
+            long sizeAv = 0;
+            int count = 1;
+            string line;
+
             Console.WriteLine("\n{0} items for {1}\n", languageResults.Count, language);
             foreach (Repository r in languageResults)
             {
+                //build csv file of repo values while outputting names to console
                 Console.WriteLine(r.FullName);
-                string line = language + "," + r.OpenIssuesCount + "," + r.ForksCount + "," + r.Size + "\n";
+                line = language + "," + r.OpenIssuesCount + "," + r.ForksCount + "," + r.Size + "\n";
                 File.AppendAllText(filePath, line);
+
+                //calculate averages as we go along
+                openIssuesAv = (r.OpenIssuesCount + (openIssuesAv * count-1)) / count;
+                ForksCountAv = (r.ForksCount + (ForksCountAv * count - 1)) / count;
+                sizeAv = (r.Size + (sizeAv * count - 1)) / count;
+
+                count++;
             }
+
+            line = language + "," + openIssuesAv + "," + ForksCountAv + "," + sizeAv + "\n";
+            File.AppendAllText(averageFP, line);
+
         }
 
         /** <summary> Reads a user's password without having it echoed to console </summary> */
